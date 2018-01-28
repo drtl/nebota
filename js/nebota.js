210acaedf6c6bc595432411dcaa3c11b373a2991
HyGlobalHD <GamerXzavier@gmail.com>
2018-01-29 01:13:26 +0800

    Update nebota.js

--- js/nebota.js
+++ js/nebota.js
@@ -1,15 +1,15 @@
         function validate() {
             var username = document.getElementById("username").value;
             var password = document.getElementById("password").value;
-            if (username == null || username == "") {
+            if (username.hashCode() == null || username.hashCode() == "") {
                 alert("Please enter the username.");
                 return false;
             }
-            if (password == null || password == "") {
+            if (password.hashCode() == null || password.hashCode() == "") {
                 alert("Please enter the password.");
                 return false;
             }
-            if (username == amplify.store("uu") || password == amplify.store("pp")){
+            if (username.hashCode() == amplify.store("uu") || password.hashCode() == amplify.store("pp")){
                 alert('Login successful');
                 return true;
             }
