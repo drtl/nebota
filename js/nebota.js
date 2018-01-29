4b0a8cfa4abfce7966d700491edd46afc7ce1808
HyGlobalHD <GamerXzavier@gmail.com>
2018-01-29 09:55:34 +0800

    Update nebota.js

--- js/nebota.js
+++ js/nebota.js
@@ -4,11 +4,11 @@
             var username = document.getElementById("username").value;
             var password = document.getElementById("password").value;
             if (username == null || username == "") {
-                alert("Please enter the username.");
+                alert("The Form Is In-Complete.");
                 return false;
             }
             if (password == null || password == "") {
-                alert("Please enter the password.");
+                alert("The Form Is In-Complete.");
                 return false;
             }
              if (username.hashCode() == store && password.hashCode() !== stone){
